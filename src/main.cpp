9fea5ef2904ad51db9613649e185d73b9f1d7e20
Eiran <eiran.amadoralvarado@gmail.com>
2021-12-06 22:26:45 -0600

    Se agrega implementacion de Hello

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@ int main() {
 
         OperacionHola *operacionHola = new OperacionHola(proveedorFormato);
         operaciones.insert(std::pair<string, Operacion *>("hola", operacionHola));
+        operaciones.insert(std::pair<string, Operacion *>("hello", operacionHola));
 
         OperacionSuma *operacionSuma = new OperacionSuma(proveedorFormato);
         operaciones.insert(std::pair<string, Operacion *>("suma", operacionSuma));
@@ -43,9 +44,11 @@ int main() {
         ProcesadorInstrucciones *procesador = new ProcesadorInstrucciones(identificadorOperaciones);
         string resultadoHola = procesador->Procese("hola", "");
         string resultadoSuma = procesador->Procese("suma", "");
+        string resultadoHello = procesador->Procese("hello", "");
 
         cout << "Resultado 'Hola': " << resultadoHola << endl;
         cout << "Resultado 'Suma': " << resultadoSuma << endl;
+        cout << "Resultado 'Hello': " << resultadoHello << endl;
 
         // Cerrar archivo de entrada
         ifs.close();
